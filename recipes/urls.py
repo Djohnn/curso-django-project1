c46588410212dbe44ec55c12a03a19eea1dee396
Johnn David <johnndavid27@gmail.com>
2023-07-20 00:03:25 -0300

    configurando urls dinamicas

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,10 @@ from . import views  # da pasta que estou importa o modulos
 
 # from recipes.views import home # sendo assim não precisamos desse import
 
+app_name = "recipes"
+
 urlpatterns = [
     # path("", view=home), usando o import comentado o caminho seria esse.
-    path("", views.home),
-    path("recipes/<int:id>/", views.recipe),
+    path("", views.home, name="home"),
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
